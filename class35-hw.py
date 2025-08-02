6e8e14e9cdbb6f208e6e10f5f4bc41c70e48ecce
Ibrahim Ismayilov <ibrahiimismayil@gmail.com>
2025-08-02 13:38:03 +0400

    implement bigger_right() func

--- class35-hw.py
+++ class35-hw.py
@@ -7,8 +7,36 @@
 
 # Question (Bigger right)
 # We look at this quesiton during the class and said that the time complexity of the solution below is n ** 2
-# However, there is a way of solving this question faster. I already gave a hint that you can solve this quesiton faster by using Stack.
+# However, there is a way of solving this question faster. I already gave a hint that you can solve
+#  this quesiton faster by using Stack.
 # Try to solve the question using stack. Describe all your steps challenges in great detail!!!!
+
+# Step 1: Create a stack implementation list and intialize it
+# Step 2: Create an empty list and intialize it to return at the end
+# Step 3: Iterate through the given list and begin with adding the first element to the stack
+# Step 4: Continue iterating through the list and adding the elements to the stack. After adding the first element, check if second element is greater than first element. If not, compare third element with second element and so forth. The priority with which element to compare changes as you add more elements to the stack
+# Step 5: If you find a greater element than top of stack, append to the empty list len(stack) amount of times. Pop as you remove from the stack
+# Step 7: Continue until the end of the list. Stop before checking the last index and append -1 to the result list
+# Step 8: Return list
+
+
+def bigger_right(lst: list) -> list:
+    stack_lst = []
+    result_lst = []
+    for idx in range(len(lst) -1):
+        stack_lst.append(lst[idx])
+        if lst[idx + 1] > stack_lst[-1]:
+            while len(stack_lst) > 0:
+                result_lst.append(lst[idx + 1])
+                stack_lst.pop()
+
+    result_lst.append(-1)
+    return result_lst
+            
+            
+        
+
+
 def bigger_right(lst):
     result = []
     for i in range(len(lst)):
