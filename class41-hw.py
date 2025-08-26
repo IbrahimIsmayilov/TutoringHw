61f30183952d09dc8cd7a736bc360416027f9ea1
Ibrahim Ismayilov <ibrahiimismayil@gmail.com>
2025-08-25 23:08:53 -0600

    implemented sum_target recursive func

--- class41-hw.py
+++ class41-hw.py
@@ -2,14 +2,47 @@
 # We have covered a reccursive quesiton sum_target in the class.
 # Complete the reccursive implementation.
 
-# Question
-# How many bits in a byte?
+# 1. figure out the smaller questions. 
+# We see if the target is equal to the first element, we look for the target in the elements preceeding the current element.
+# Target changes if the first element is smaller than the target. New target becomes the old target - the first element
+# 2. when you find an answer to smaller questions, how do u use them to find the answer to the current question.
+# You or the results of the smaller questions.
+# 3. when do we stop? When the target reaches 0.1
+
+def sum_target(lst: list[int], target: int) -> bool:
+    
+    if target == 0:
+        return True
+    elif target < 0:
+        return False
+    elif len(lst) == 0 and target > 0:
+        return False
+         
+        
+    
+    q1 = sum_target(lst[1:], target)
+    q2 = sum_target(lst[1:], target - lst[0])
+
+    return q1 or q2
+
+
+print(sum_target([10,1,1,4], 5))  # True
+
+    
+
+
+
+
 
 # Question
-# How many bits in 7 bytes?
+# How many bits in a byte? There are 8 bits in a byte. 
+
+# Question
+# How many bits in 7 bytes? There are 56 bits in 7 bytes. 
 
 # Question
 # How many bytes are needed to store a string in memory?
+# 
 # How many bytes are needed to store an integer in memory?
 # How many bytes are needed to store a boolean in memory?
 # How many bytes in memory does each of the following variables occupy?
@@ -21,6 +54,7 @@ a = ["Osman", 54, 61]
 
 # Question
 # Which one is faster: RAM or Storage?
+# RAM is much faster, has to deal with less data and can search through much faster. 
 
 # Question
 # Why do we have memory and storage in the system?
